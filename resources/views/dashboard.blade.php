3cf09b7fce233b73b22bdf9954cad5d9f3ad92a1
CLBRTT <jjepsani1@gmail.com>
2022-07-08 16:56:44 +0800

    Another Function

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
 <div class="main-content">
     <div class="dashboard">
         <h1>DASHBOARD</h1>
-        <button action="{{route('Logout')}}" method="get" class="btn btn-lg btn-primary btn-block" type="logout">LOG OUT</button>
+        <button type="button" action="{{route('Logout')}}" method="get" class="butt1 btn btn-primary" type="logout">LOG OUT</button>
     </div>
 </div>
 @endsection
--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -31,3 +31,35 @@
         </div>
     </div>
 </div>
+
+
+
+<!--FOR REGISTRATION -->
+<div class="modal fade" id="exampleregister" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="form-register" action="{{route('Sampleregister')}}" method="post">
+                    @csrf
+                    <label for="InputEmail" class="sr-only">EMAIL</label>
+                    <input type="text" name="email" id="InputEmail" class="form-control" placeholder="Email" required=""
+                        autofocus="">
+                    <br>
+                    <label for="InputPass" class="sr-only">PASSWORD</label>
+                    <input type="password" name="pass" id="InputPass" class="form-control" placeholder="Password"
+                        required="">
+                    <br>
+                    <button class="btn btn-lg btn-primary btn-block" type="Register">SUBMIT</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
