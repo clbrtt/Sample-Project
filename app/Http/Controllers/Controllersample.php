60913dcf15c493107bd1081a0172cba434f5231d
Rallion Laynes <ralphlaynes@gmail.com>
2022-07-08 10:16:31 +0800

    updated controller added

--- app/Http/Controllers/Controllersample.php
+++ app/Http/Controllers/Controllersample.php
@@ -36,6 +36,10 @@ class Controllersample extends Controller {
             return redirect()->route('login');
         }
     }
+
+    public function semi_ut(){
+        return 'semi';
+    }
         
 
 
