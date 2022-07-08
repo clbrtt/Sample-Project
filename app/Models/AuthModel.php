49329cb98ac9ed7b4ecc87881881cb94356ea167
Rallion Laynes <ralphlaynes@gmail.com>
2022-07-08 11:24:08 +0800

    Rallion Update

--- app/Models/AuthModel.php
+++ app/Models/AuthModel.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class AuthModel extends Model
+
+
+
+
+class AuthModel extends Authenticatable
 {
     use HasFactory;
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $primaryKey = 'Id';
+
+    protected $fillable = [
+        'Email', 'Password'
+    ];
 }
